6348212fbb855c4cd9166930316e1518d0464a9f
IgorShkidchenko <igorshkidchenko@gmail.com>
2018-12-10 15:13:14 +0200

    add_'_'_to_errors_name

--- app/modules/errors/errors.rb
+++ app/modules/errors/errors.rb
@@ -3,19 +3,19 @@
 module Errors
   class CoverError < StandardError
     def initialize
-      super(I18n.t('exceptions.covererror'))
+      super(I18n.t('exceptions.cover_error'))
     end
   end
 
   class IncludeError < StandardError
     def initialize
-      super(I18n.t('exceptions.includeerror'))
+      super(I18n.t('exceptions.include_error'))
     end
   end
 
   class SizeError < StandardError
     def initialize
-      super(I18n.t('exceptions.sizeerror'))
+      super(I18n.t('exceptions.size_error'))
     end
   end
 end
